e955bdcb4edfed93daab3bbdcdf0c3edc0c942b3
Mickman <josh.abramoff@rutgers.edu>
2021-12-29 19:32:29 -0500

    reddit video downloading, domain differentiation

--- backend/js/index.js
+++ backend/js/index.js
@@ -12,7 +12,6 @@ async function getTweet(id) {
 			case 'animated_gif':
 				return tweet.extended_entities.media[0].video_info.variants[0].url;
 		}
-		
   })
   .catch(function (error) {
 		//throw error
@@ -40,7 +39,56 @@ function getHighestQualityURL(videosArray) {
 	return videosArray[highestQualityVideoBitrateIndex].url;
 }
 
-function getTwitterCredentials() {
+function getRedditVideoURL(requestURL) {
+	// Fetch JSON from Reddit URL and return video link
+	// Example: https://v.redd.it/pvenqawy7bk71
+	let fetch = require('node-fetch')
+	let settings = { method: 'Get' }
+
+	return new Promise(resolve => {
+		fetch(requestURL, settings)
+			.then(res => res.json())
+				.then((json) => {
+					// Reddit JSON for if a post is a video
+					if (json[0].data.children[0].data.is_video)
+						resolve(json[0].data.children[0].data.secure_media.reddit_video.fallback_url)
+				})
+	});
+}
+
+function combineVideoAndAudio(videoURL, audioURL, fileName) {
+	var fs = require('fs')
+
+	if (fs.existsSync(`/var/www/html/reddit/${fileName}.mp4`)) {
+		console.log('File Exists!')
+		return `https://mackmin.me/reddit/${fileName}.mp4`;
+	}
+	// Using spawn to run FFmpeg CLI command to combine DASH_1080 and DASH_audio
+	var spawn = require('child_process').spawn
+
+	var cmd = '/usr/bin/ffmpeg'
+
+	// FFmpeg CLI arguments to add audio track to video
+	var args = [
+		'-i', videoURL,
+		'-i', audioURL,
+		'-map', '0:v',
+		'-map', '1:a',
+		'-c:v', 'copy',
+		'-shortest',
+		'-f', 'mp4', `/var/www/html/reddit/${fileName}.mp4`
+	]
+
+	var proc = spawn(cmd, args)
+
+	proc.stderr.setEncoding('utf8')
+
+	return new Promise(resolve => {
+		proc.on('close', () => resolve(`https://mackmin.me/reddit/${fileName}.mp4`))
+	});
+}
+
+function getTwitterClient() {
 	const Twitter = require('twitter')
 	const twitterTokens = require('../config/twitter_auth_tokens.json')
 
@@ -54,7 +102,7 @@ function getTwitterCredentials() {
 	return twitterClient;
 }
 
-function getRedditCredentials() {
+function getRedditClient() {
 	const snoowrap = require('snoowrap')
 	const redditTokens = require('../config/reddit_auth_tokens.json')
 
@@ -68,19 +116,56 @@ function getRedditCredentials() {
 	return r;
 }
 
+function getYouTubeClient() {
+	const ytdl = require('ytdl-core')
+	return ytdl;
+}
+
+async function getYouTubeVideo(videoURL) {
+	youTubeClient(videoURL, { filter: 'audioonly', format: 'mp3' })
+		.pipe(fs.createWriteStream('/var/www/html/assets/newvid.mp3'))
+		return setTimeout(function() {
+			return 'https://mickman.tech/assets/newvid.mp3';
+		}, 3000)
+}
+
 async function determineService(requestDomain, requestURL) {
 	switch (requestDomain) {
+		//TODO: Don't check for all subdomains, just the main domain
+		// Example: *.twitter.com, *.reddit.com
 		case 'twitter.com':
+		case 'mobile.twitter.com':
 			let tweetID = /[^/]*$/.exec(requestURL)[0]
-			let responseURL = await getTweet(tweetID)
+			var responseURL = await getTweet(tweetID)
+			console.log(new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true }) + '\nDomain: Twitter\n')
 			console.log(responseURL)
+
 			return responseURL;
 		case 'www.reddit.com':
-			console.log('reddit')
-			break
+		case 'new.reddit.com':
+		case 'old.reddit.com':
+			console.log(new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true }) + '\nDomain: Reddit\n')
+			console.log(requestURL)
+
+			let redditVideoURL = await getRedditVideoURL(`${requestURL}.json`)
+			let redditPostLink = redditVideoURL.substring(0, redditVideoURL.search('/DASH_'))
+			let redditFileName = redditPostLink.substring(redditPostLink.lastIndexOf('/') + 1)
+			var redditAudioURL = redditPostLink + '/DASH_audio.mp4'
+			var responseURL = await combineVideoAndAudio(redditVideoURL, redditAudioURL, redditFileName)
+			return responseURL;
+		case 'www.youtube.com':
+			console.log('\nDomain: YouTube')
+
+			var responseURL = await getYouTubeVideo(requestURL)
+
+			return responseURL;
 		default:
-			console.log('other')
+			console.log('\nDomain: Other')
+			// Include 422 Error for other domain
+			return;
 	}
+
+	//console.log(object)
 }
 
 const getHostnameFromRegex = (url) => {
@@ -103,14 +188,16 @@ async function startServer() {
 	app.use(bodyParser.json())
 
 	app.post('/', async (req, res) => {
-		//let startTime = Date.now()
-		//console.log(req.body)
-		
+		// Checking for URL in request body
+		if (!req.body.url) {
+			return res.status(400).end('no_url');
+		}
+
 		//let url = await getTweet(req.body.id)
 		let requestDomain = getHostnameFromRegex(req.body.url)
 		let responseURL = await determineService(requestDomain, req.body.url)
 
-		if (responseURL)
+		//if (responseURL)
 		//console.log(req.body.url)
 
 		if (responseURL === 'no_post') {
@@ -118,16 +205,17 @@ async function startServer() {
 		}
 
 		res.status(200).end(responseURL.toString())
-		//let endTime = Date.now()
-		//console.log(endTime - startTime)
+		console.log('Response Sent')
 	})
 
 	// Start express on the defined port
 	app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 }
 
-const twitterClient = getTwitterCredentials()
-const redditClient = getRedditCredentials()
+const twitterClient = getTwitterClient()
+//const redditClient = getRedditClient()
+//const youTubeClient = getYouTubeClient()
+const fs = require('fs')
 //redditClient.getSubmission('o7h5lh').url.then(post => console.log(post))
 
 startServer()
